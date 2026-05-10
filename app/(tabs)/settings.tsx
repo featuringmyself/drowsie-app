00c3e878b70bf1ed57bd0e36625e44f6210aae39
Rudra <rudralocking@gmail.com>
2026-05-10 12:51:15 +0530

    feat: enhance Settings screen with StarrySky background and improved layout for better user experience

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,9 +1,17 @@
-import { Text, View } from "react-native";
+import { StarrySky } from "@/src/components/StarrySky";
+import { StyleSheet, Text, View, useWindowDimensions } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Settings() {
+    const { width, height } = useWindowDimensions();
     return (
-        <View className="flex-1 items-center justify-center bg-night">
-            <Text className="font-sans text-star">Settings</Text>
+        <View className="flex-1 bg-night">
+            <View style={StyleSheet.absoluteFill}>
+                <StarrySky width={width} height={height} />
+            </View>
+            <SafeAreaView className="relative z-10">
+                <Text className="font-sans text-star mt-4 text-center text-3xl font-medium">Settings</Text>
+            </SafeAreaView>
         </View>
     )
 }
